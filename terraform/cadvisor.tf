c9f5ac1859cf4d3c72500eedfe35379bc3b91f3a
vtiftilov <vlad.tiftilov@yahoo.com>
2024-11-19 07:49:16 +0200

    Refactor cAdvisor deployment to use DaemonSet and add necessary volume mounts

--- terraform/cadvisor.tf
+++ terraform/cadvisor.tf
@@ -1,22 +1,20 @@
-
-// cAdvisor Deployment
+// cAdvisor Service Account
 resource "kubernetes_service_account" "cadvisor_sa" {
   metadata {
     name = "cadvisor-sa"
   }
 }
 
-resource "kubernetes_deployment" "cadvisor" {
+// cAdvisor DaemonSet
+resource "kubernetes_daemonset" "cadvisor" {
   metadata {
-    name = "cadvisor-deployment"
+    name = "cadvisor-daemonset"
     labels = {
       app = "cadvisor"
     }
   }
 
   spec {
-    replicas = 1
-
     selector {
       match_labels = {
         app = "cadvisor"
@@ -36,7 +34,6 @@ resource "kubernetes_deployment" "cadvisor" {
         container {
           name  = "cadvisor"
           image = "gcr.io/cadvisor/cadvisor:latest"
-
           image_pull_policy = "IfNotPresent"
 
           port {
@@ -53,6 +50,71 @@ resource "kubernetes_deployment" "cadvisor" {
               cpu    = "125m"
             }
           }
+
+          volume_mount {
+            name       = "rootfs"
+            mount_path = "/rootfs"
+            read_only  = true
+          }
+
+          volume_mount {
+            name       = "var-run"
+            mount_path = "/var/run"
+            read_only  = false
+          }
+
+          volume_mount {
+            name       = "sys"
+            mount_path = "/sys"
+            read_only  = true
+          }
+
+          volume_mount {
+            name       = "docker"
+            mount_path = "/var/lib/docker"
+            read_only  = true
+          }
+
+          volume_mount {
+            name       = "cgroup"
+            mount_path = "/sys/fs/cgroup"
+            read_only  = true
+          }
+        }
+
+        volume {
+          name = "rootfs"
+          host_path {
+            path = "/"
+          }
+        }
+
+        volume {
+          name = "var-run"
+          host_path {
+            path = "/var/run"
+          }
+        }
+
+        volume {
+          name = "sys"
+          host_path {
+            path = "/sys"
+          }
+        }
+
+        volume {
+          name = "docker"
+          host_path {
+            path = "/var/lib/docker"
+          }
+        }
+
+        volume {
+          name = "cgroup"
+          host_path {
+            path = "/sys/fs/cgroup"
+          }
         }
       }
     }
@@ -63,6 +125,7 @@ resource "kubernetes_deployment" "cadvisor" {
   ]
 }
 
+// cAdvisor Service
 resource "kubernetes_service" "cadvisor_service" {
   metadata {
     name = "cadvisor-service"
